14750f5b104ee22844c2b8d19648ca2a78217718
DamiaoCanndido <tower_gamer@hotmail.com>
2024-05-06 18:33:37 -0300

    add express and ejs

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as dotenv from 'dotenv';
+import express from 'express';
 import axios from 'axios';
 import https from 'https';
 import fs from 'fs';
@@ -19,45 +20,53 @@ const credentials = Buffer.from(
   `${process.env.EFI_CLIENT_ID}:${process.env.EFI_CLIENT_SECRET}`
 ).toString('base64');
 
-axios({
-  method: 'POST',
-  url: `${process.env.EFI_URL}/oauth/token`,
-  headers: {
-    Authorization: `Basic ${credentials}`,
-    'Content-Type': 'application/json',
-  },
-  httpsAgent: agent,
-  data: {
-    grant_type: 'client_credentials',
-  },
-}).then((response) => {
-  const accessToken = response.data?.access_token;
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
 
-  const reqEFI = axios.create({
-    baseURL: process.env.EFI_URL,
-    httpsAgent: agent,
+app.get('/', (req, res) => {
+  axios({
+    method: 'POST',
+    url: `${process.env.EFI_URL}/oauth/token`,
     headers: {
-      Authorization: `Bearer ${accessToken}`,
+      Authorization: `Basic ${credentials}`,
       'Content-Type': 'application/json',
     },
+    httpsAgent: agent,
+    data: {
+      grant_type: 'client_credentials',
+    },
+  }).then((response) => {
+    const accessToken = response.data?.access_token;
+
+    const reqEFI = axios.create({
+      baseURL: process.env.EFI_URL,
+      httpsAgent: agent,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const dataCob = {
+      calendario: {
+        expiracao: 3600,
+      },
+      devedor: {
+        cpf: '12345678909',
+        nome: 'Francisco da Silva',
+      },
+      valor: {
+        original: '10.00',
+      },
+      chave: '160126bd-136d-4c38-b408-e600b6f56a23',
+      solicitacaoPagador: 'Cobrança dos serviços prestados.',
+    };
+
+    reqEFI.post('/v2/cob', dataCob).then((response) => res.send(response.data));
   });
+});
 
-  const dataCob = {
-    calendario: {
-      expiracao: 3600,
-    },
-    devedor: {
-      cpf: '12345678909',
-      nome: 'Francisco da Silva',
-    },
-    valor: {
-      original: '10.00',
-    },
-    chave: '160126bd-136d-4c38-b408-e600b6f56a23',
-    solicitacaoPagador: 'Cobrança dos serviços prestados.',
-  };
-
-  reqEFI
-    .post('/v2/cob', dataCob)
-    .then((response) => console.log(response.data));
+app.listen(5555, () => {
+  console.log('Server running on port 5555...');
 });
