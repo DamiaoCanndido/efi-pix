cac1d2b9e3d5e1448fd0d602972e4116ecace8e6
DamiaoCanndido <tower_gamer@hotmail.com>
2024-05-06 21:25:52 -0300

    refactor async await

--- src/main.ts
+++ src/main.ts
@@ -24,8 +24,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
 
-app.get('/', (req, res) => {
-  axios({
+app.get('/', async (req, res) => {
+  const authResponse = await axios({
     method: 'POST',
     url: `${process.env.EFI_URL}/oauth/token`,
     headers: {
@@ -36,35 +36,36 @@ app.get('/', (req, res) => {
     data: {
       grant_type: 'client_credentials',
     },
-  }).then((response) => {
-    const accessToken = response.data?.access_token;
-
-    const reqEFI = axios.create({
-      baseURL: process.env.EFI_URL,
-      httpsAgent: agent,
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        'Content-Type': 'application/json',
-      },
-    });
-
-    const dataCob = {
-      calendario: {
-        expiracao: 3600,
-      },
-      devedor: {
-        cpf: '12345678909',
-        nome: 'Francisco da Silva',
-      },
-      valor: {
-        original: '10.00',
-      },
-      chave: '160126bd-136d-4c38-b408-e600b6f56a23',
-      solicitacaoPagador: 'Cobrança dos serviços prestados.',
-    };
-
-    reqEFI.post('/v2/cob', dataCob).then((response) => res.send(response.data));
   });
+  const accessToken = authResponse.data?.access_token;
+
+  const reqEFI = axios.create({
+    baseURL: process.env.EFI_URL,
+    httpsAgent: agent,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const dataCob = {
+    calendario: {
+      expiracao: 3600,
+    },
+    devedor: {
+      cpf: '12345678909',
+      nome: 'Francisco da Silva',
+    },
+    valor: {
+      original: '10.00',
+    },
+    chave: '160126bd-136d-4c38-b408-e600b6f56a23',
+    solicitacaoPagador: 'Cobrança dos serviços prestados.',
+  };
+
+  const cobResponse = await reqEFI.post('/v2/cob', dataCob);
+
+  res.send(cobResponse.data);
 });
 
 app.listen(5555, () => {
